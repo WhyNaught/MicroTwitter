fb64efeb26cf5b0ab56c662ad3c314cb6fb4ea85
Umar <umardxbfarooq@gmail.com>
2025-01-03 22:24:06 -0500

    Initialized db tables

--- init.sql
+++ init.sql
@@ -1,21 +1,29 @@
+CREATE DATABASE IF NOT EXISTS microtweets; 
 CREATE TABLE users(
     id SERIAL NOT NULL PRIMARY KEY, 
-    username varchar(50) NOT NULL, 
-    email varchar(50) NOT NULL, 
+    username varchar(50) NOT NULL UNIQUE, 
+    email varchar(50) NOT NULL UNIQUE, 
     password varchar(50) NOT NULL,
     first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL
+    last_name varchar(50) NOT NULL, 
 );  
 
 CREATE TABLE posts(
     id SERIAL NOT NULL PRIMARY KEY,
-    body varchar(255),  
-    author varchar(255) NOT NULL, 
-    post_date varchar(255) NOT NULL
+    author_id INT NOT NULL 
+    title varchar(50), 
+    body varchar(255),
+    likes INT NOT NULL DEFAULT 0, 
+    comment_count INT NOT NULL DEFAULT 0, 
+    post_date varchar(255) NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments(
     id SERIAL NOT NULL PRIMARY KEY, 
-    author varchar(255) NOT NULL, -- change this maybe
+    author_id INT NOT NULL, 
+    body varchar(255) NOT NULL, 
+    likes INT NOT NULL DEFAULT 0, 
     comment_date varchar(255) NOT NULL, 
-)
+    FOREIGN KEY (author_id) REFERENCES users(id)
+); 
