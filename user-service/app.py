625cd6a59befbeeb484fb26698a360fe93ab325e
Umar <umardxbfarooq@gmail.com>
2025-01-06 10:47:38 -0500

    Fixed login route

--- user-service/app.py
+++ user-service/app.py
@@ -2,8 +2,9 @@
 import os 
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required 
 
 load_dotenv()
 
@@ -12,7 +13,10 @@ url = os.getenv('DB_URL')
 connection = psycopg2.connect(url)
 bcrypt = Bcrypt(app) 
 
-register_query = """INSERT INTO users (username, email, password, first_name, last_name) VALUES (%s, %s, %s, %s, %s)"""
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
+app.config["JWT_TOKEN_LOCATION"] = ['headers']
+
 @app.route('/register', methods = ['POST'])
 def register():
     try:
@@ -25,6 +29,7 @@ def register():
         last_name = data["last_name"]
         with connection:
             with connection.cursor() as cursor:
+                register_query = '''INSERT INTO users (username, email, password, first_name, last_name) VALUES (%s, %s, %s, %s, %s)'''
                 cursor.execute(register_query, (username, email, hashed_password, first_name, last_name))
         return {"message": "user registered successfully"}, 201
     except Exception as e:
@@ -34,8 +39,20 @@ def register():
 def login():
     try:
         data = request.get_json()
-        username = data["username"]
+        email = data["email"]
         password_guess = data["password"]
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''SELECT * FROM users WHERE email = %s''', (email, ))
+                user = cursor.fetchone()
+            if user:
+                if bcrypt.check_password_hash(user[3], password_guess):
+                    #return jwt, but for now just a status code 
+                    return jsonify({"message" : "logged in succesfully!"}), 200
+                else: 
+                    return jsonify({"message" : "incorrect email or password"}), 400    
+            else:
+                return jsonify({"message" : "incorrect email or password"}), 400
     except Exception as e:
         return {"error": str(e)}, 500
     
